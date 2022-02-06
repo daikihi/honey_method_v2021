d95add511498c6ce21c848cb785fb7bf8ab3f9dc
daiki <daikihi+github@gmail.com>
2022-02-06 11:12:03 +0900

    add an index.html fil and call js from index

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use actix_web::{HttpServer, App};
+use actix_web::{HttpServer, App, web};
+use actix_files::NamedFile;
+use actix_web::{HttpRequest, Result};
+use actix_files as fs;
+use std::path::PathBuf;
+
+
+
+async fn index(_: HttpRequest) -> Result<NamedFile> {
+    let path: PathBuf = "./src/static/index.html".parse().unwrap();
+    Ok(NamedFile::open(path)?)
+}
 
 #[actix_web::main]
 async fn main()  -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            
+        .route("/web/index.html", web::get().to(index))
+        .service(fs::Files::new("/web/static/scripts/", "./src/static/scripts/").show_files_listing())
     })
     .bind("127.0.0.1:8080")?
     .run()
