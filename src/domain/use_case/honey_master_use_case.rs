2c3f153b22f2c8ceff4956fa9d5a276678eb412a
daiki <daikihi+github@gmail.com>
2022-02-19 15:31:09 +0900

    dummy api responses dummy data

--- src/domain/use_case/honey_master_use_case.rs
+++ src/domain/use_case/honey_master_use_case.rs
@@ -1,6 +1,29 @@
-use crate::domain::models::honeies::Honey;
+use crate::domain::models::{honeies::Honey, times::Month, beekeepers::Beekeeper};
 
 pub async fn get_all_honeies() -> Vec<Honey>{
     // @todo should call to HMS API or DB repository
-    vec![]
+    get_dummy_honeies()
+}
+
+
+fn get_dummy_honeies() -> Vec<Honey> {
+    vec![
+        Honey{
+            name: crate::domain::models::honeies::HoneyName { name: "里山のはちみつ".to_string() },
+            year: crate::domain::models::times::Year { value: 2020 },
+            month: Some(crate::domain::models::times::Month{ value: 7}),
+            beekeeper: crate::domain::models::beekeepers::Beekeeper{},   
+            prefecture: None,
+            flowers: vec![]
+        },        
+        Honey{
+            name: crate::domain::models::honeies::HoneyName { name: "桜の蜂蜜".to_string() },
+            year: crate::domain::models::times::Year { value: 2020 },
+            month: Some(crate::domain::models::times::Month{ value: 4}),
+            beekeeper: crate::domain::models::beekeepers::Beekeeper{},   
+            prefecture: None,
+            flowers: vec![]
+        }
+
+    ]
 }
